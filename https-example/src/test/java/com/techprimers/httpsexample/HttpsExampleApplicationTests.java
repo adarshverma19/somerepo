a5bf6e85ebd70ee8ae9cedc0e93745d96d97abab
adarshverma19 <47151157+adarshverma19@users.noreply.github.com>
2020-10-05 18:17:08 +0530

    Add files via upload
    Basic Springboot Project

---
+++ https-example/src/test/java/com/techprimers/httpsexample/HttpsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.httpsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
